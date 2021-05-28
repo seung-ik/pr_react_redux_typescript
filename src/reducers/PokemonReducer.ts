6644f13cd61e49fd4a8b1d528cb250f63122b0ff
seung-ik <69374857+seung-ik@users.noreply.github.com>
2021-05-28 22:33:16 +0900

    redux store setting

---
+++ src/reducers/PokemonReducer.ts
@@ -0,0 +1,16 @@
+interface initialState{
+
+}
+const initialState:initialState={
+
+}
+
+const PokenmonReducer = (state=initialState,action:any)=>{
+  switch(action.type){
+
+    default:
+      return state
+  }
+}
+
+export default PokenmonReducer
---
+++ src/reducers/index.ts
@@ -0,0 +1,8 @@
+import PokenmonReducer from './PokemonReducer'
+import{ combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  PokenmonReducer
+})
+
+export default rootReducer
